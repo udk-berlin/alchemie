dadf99b1e2648169258b72d48ff8a200092fb3c8
Felix <>
2024-05-04 12:37:53 +0200

    basic html structure header footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <div className="header">
+          <h1>Alchemie der Udk</h1>
+          <h1>Universität der Künste Berlin</h1>
+        </div>
+
+        <div className="scrollContainer">
+          <img src="./images/image.png"></img>
+        </div>
+
+        <footer>
+          <div className="container">
+            <h1>Kontakt</h1>
+          </div>
+          <div className="container">
+            <h1>Impressum</h1>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
