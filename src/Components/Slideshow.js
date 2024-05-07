e84f8bf96dabec2fb3c56c717cbd8b8a29978e57
Felix <55440403+muellerfx@users.noreply.github.com>
2024-05-07 18:55:31 +0200

    added process component

--- src/Components/Slideshow.js
+++ src/Components/Slideshow.js
@@ -1,17 +1,31 @@
-import "../Styles/Slideshow.scss"
+import "../Styles/Slideshow.scss";
 import React from "react";
 
 class Slidehow extends React.Component {
   constructor() {
     super();
   }
+
+  handleClick(){
+      window.scroll({
+        top: document.body.offsetHeight,
+        left: 0, 
+        behavior: 'smooth',
+      });
+  }
+
   render() {
     return (
-        <div className="slideShowContainer">
-            <img alt="" src="./images/image.png"></img>
-            <img alt="" className="fade" src="./images/image1.png"></img>
-            <div className="overlay"></div>
+      <div className="slideShowContainer">
+        <img alt="" src="./images/image.png"></img>
+        <img alt="" className="fade" src="./images/image1.png"></img>
+        <div className="overlay"></div>
+
+        <div onClick={()=> this.handleClick()} className="arrowContainer">
+          <p>zum Prozess</p>
+          <p>↓</p>
         </div>
+      </div>
     );
   }
 }
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,6 +1,8 @@
 import "../App.scss";
 import React from "react";
 import Slidehow from "../Components/Slideshow";
+import Process from "../Components/Process";
+// import Main from "../Styles/Main.scss"
 
 
 class Main extends React.Component {
@@ -9,7 +11,10 @@ class Main extends React.Component {
 
   render() {
     return (
+      <>
       <Slidehow></Slidehow>
+      <Process></Process>
+      </>
     );
   }
 }
