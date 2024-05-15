3e7b52649627dff6ca6c4765f5cb64d46c1cee82
Felix <55440403+muellerfx@users.noreply.github.com>
2024-05-15 17:54:32 +0200

    basic archive module

--- src/Components/Archive.js
+++ src/Components/Archive.js
@@ -2,13 +2,42 @@ import "../Styles/Archive.scss";
 import React from "react";
 // import Markdown from "react-markdown";
 
+const archiveItem = ['LOREM IPSUM', 'GUBERGEN', 'NONUMY', 'SANCTUS']; 
+
 class Archive extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      activeMenuItem : ''
+    }
+  }
+
+  changeAcitveItem(i){
+    this.setState({activeMenuItem : i}); 
+  }
+  
   render() {
     return (
       <>
         <div className="headline">{this.props.language === "DE" ? "Projekte" : "Projects"}</div>
         <div className="archiveContainer">
-          <div className="archiveContent">
+          <div className="archiveNav">
+
+            {
+              archiveItem.map((item) => 
+                  <div onClick={()=> this.changeAcitveItem(item)} className="navItem" style={{background: this.state.activeMenuItem === item ? 'red' : 'lightgrey'}}>{item}</div>    
+              )
+            }
+
+          </div>
+          <div className="archive">
+            <div className="archiveItem"></div>
+            <div className="archiveItem"></div>
+            <div className="archiveItem"></div>
+            <div className="archiveItem"></div>
+            <div className="archiveItem"></div>
+            <div className="archiveItem"></div>
+            <div className="archiveItem"></div>
           </div>
         </div>
       </>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,12 +11,16 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <h1><Link to="/">{this.props.language === 'DE' ? 'Alchemie der UdK' : 'Alchemy of the UdK'}</Link></h1>
-        <div className="languageSelector"  style={{ display : this.props.windowWidth < 700 ? 'none' : 'flex'}}>
+        <h1><Link to="/">{this.props.language === 'DE' && this.props.windowWidth > 700 ? 'Alchemie der UdK' : this.props.language === 'EN' && this.props.windowWidth > 700 ? 'Alchemy of the Udk' : this.props.language === 'DE' && this.props.windowWidth < 700 ? 'Alchemie der Universität der Künste Berlin' : 'Alchemy at the Berlin University of the Arts'}</Link></h1>
+        <div className="languageSelector">
           <h1 onClick={()=> this.props.changeLanguage('DE')} style={{color: this.props.language === 'DE' ? 'black' : 'lightgrey'}}>DE</h1>
           <h1 onClick={()=> this.props.changeLanguage('EN')} style={{color: this.props.language === 'EN' ? 'black' : 'lightgrey'}}>EN</h1>
         </div>
-        <h1>{this.props.language === 'DE' ? 'Universität der Künste Berlin' : 'Berlin University of the Arts'}</h1>
+        {
+          this.props.windowWidth > 700 ? 
+          <h1>{this.props.language === 'DE' ? 'Universität der Künste Berlin' : 'Berlin University of the Arts'}</h1>
+          : null
+        }
       </div>
     );
   }
