b5839423b43a61a65b0a081146f74f18a4c19816
Felix <55440403+muellerfx@users.noreply.github.com>
2024-05-28 23:22:47 +0200

    added test images

--- src/Components/Archive.js
+++ src/Components/Archive.js
@@ -1,42 +1,45 @@
 import "../Styles/Archive.scss";
 import React from "react";
 
-const archiveItem = ['LOREM IPSUM', 'GUBERGEN', 'NONUMY', 'SANCTUS']; 
+const archiveItem = ["Freiheit", "Neugier", "Kollaborative Kreativität", "Zuhören", "Gerechtigkeit/Verteilung"];
 
 class Archive extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      activeMenuItem : ''
-    }
+      activeMenuItem: "",
+    };
   }
 
-  changeAcitveItem(i){
-    this.setState({activeMenuItem : i}); 
+  changeAcitveItem(i) {
+    this.setState({ activeMenuItem: i });
   }
-  
+
   render() {
     return (
       <>
         <div className="headline">{this.props.language === "DE" ? "Projekte" : "Projects"}</div>
         <div className="archiveContainer">
           <div className="archiveNav">
-
-            {
-              archiveItem.map((item) => 
-                  <div onClick={()=> this.changeAcitveItem(item)} className="navItem" style={{background: this.state.activeMenuItem === item ? 'red' : 'lightgrey'}}>{item}</div>    
-              )
-            }
-
+            {archiveItem.map((item) => (
+              <div onClick={() => this.changeAcitveItem(item)} className="navItem" style={{ background: this.state.activeMenuItem === item ? "red" : "lightgrey" }}>
+                {item}
+              </div>
+            ))}
           </div>
           <div className="archive">
-            <div className="archiveItem"></div>
-            <div className="archiveItem"></div>
-            <div className="archiveItem"></div>
-            <div className="archiveItem"></div>
-            <div className="archiveItem"></div>
-            <div className="archiveItem"></div>
-            <div className="archiveItem"></div>
+            <div className="archiveItem">
+              <img src="./images/assemblage.png"></img>
+            </div>
+            <div className="archiveItem">
+              <img src="./images/grundrauschen.png"></img>
+            </div>
+            <div className="archiveItem">
+              <img src="./images/strang.png"></img>
+            </div>
+            <div className="archiveItem">
+              <img src="./images/zuhoren.png"></img>
+            </div>
           </div>
         </div>
       </>
