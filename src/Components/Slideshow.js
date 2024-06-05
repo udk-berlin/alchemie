a51831796e66e5520da0f24929fa07692afbffbd
Felix <55440403+muellerfx@users.noreply.github.com>
2024-06-05 17:11:50 +0200

    landing page + markdown files + headimage

--- src/Components/Slideshow.js
+++ src/Components/Slideshow.js
@@ -4,27 +4,44 @@ import React from "react";
 class Slidehow extends React.Component {
   constructor() {
     super();
+    this.state = {
+      textDE : '',
+      textEN : ''
+    }
   }
 
-  handleClick(){
-      window.scroll({
-        top: document.body.offsetHeight,
-        left: 0, 
-        behavior: 'smooth',
-      });
+  handleClick() {
+    window.scroll({
+      top: document.body.offsetHeight,
+      left: 0,
+      behavior: "smooth",
+    });
   }
+
+  
+    componentDidMount(){
+      fetch("./data/landingDE.md")
+      .then((r) => r.text())
+      .then(text => {
+        this.setState({textDE : text}); 
+      });
+  
+      fetch("./data/landingEN.md")
+      .then((r) => r.text())
+      .then(text => {
+        this.setState({textEN : text}); 
+      });
+    }
+  
 
   render() {
     return (
       <div className="slideShowContainer">
-        <img alt="" src="./images/image.png"></img>
-        <img alt="" className="fade" src="./images/image1.png"></img>
+        <img alt="" src="./images/headImage.jpg"></img>
         <div className="overlay"></div>
-
-        {/* <div onClick={()=> this.handleClick()} className="arrowContainer">
-          <p>zum Prozess</p>
-          <p>↓</p>
-        </div> */}
+        <div className="textOverlay">
+          <h1>{this.props.language === 'DE' ? this.state.textDE : this.state.textEN}</h1>
+        </div>
       </div>
     );
   }
